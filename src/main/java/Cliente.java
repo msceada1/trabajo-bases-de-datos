b5e0b33fc40e3361b7000c4b546dcd63f95ba9d4
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-06 17:35:07 +0200

    creando la clase Cliente

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+
+public class Cliente {
+
+    private String idCliente;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+
+    public Cliente(String idCliente, String nombre, String apellido1, String apellido2, int edad) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente='" + idCliente + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido1='" + apellido1 + '\'' +
+                ", apellido2='" + apellido2 + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Cliente cliente)) return false;
+        return Objects.equals(idCliente, cliente.idCliente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idCliente);
+    }
+}
