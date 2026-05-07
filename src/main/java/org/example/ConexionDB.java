846a5f226d84536197eaf9b6053f9b368df28f13
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-07 09:18:29 +0200

    moviendo clases a la carpeta org para evitar fallos de deteccion

--- src/main/java/org/example/ConexionDB.java
+++ src/main/java/org/example/ConexionDB.java
@@ -1,6 +1,7 @@
+package org.example;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConexionDB {
     private static Connection conexion = null;
--- src/main/java/org/example/PropertiesReader.java
+++ src/main/java/org/example/PropertiesReader.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import exceptions.AppException;
 
 import java.io.BufferedReader;
