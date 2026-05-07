bf7d6d065c63b3cb6eabdf267085f520c0369533
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-07 18:40:03 +0200

    creando la interfaz VentaDAO

--- src/main/java/org/example/VentaDAO.java
+++ src/main/java/org/example/VentaDAO.java
@@ -1,5 +1,16 @@
 package org.example;
 
-public interface VentaDAO {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface VentaDAO {
+    void insertar(Venta v);
+
+    Venta obtenerPorFormaDePago(String formaDePago);
+
+    List<Venta> obtenerVentas();
+
+    void actualizar(Venta v);
+
+    void eliminarPorFecha(LocalDate fechaDeVenta);
 }
